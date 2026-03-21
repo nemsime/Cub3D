0c23522358f1d9b93ccf3f26a1fa8c47b5fa25eb
Thomas Edward <probably.not.a.number@gmail.com>
2026-03-21 19:42:54 +0000

    p

--- src/main.c
+++ src/main.c
@@ -5,9 +5,6 @@ int	main(int argc, char **argv)
 	t_game game;
 	// validation_stage(argc, argv, &game);
 	(void)argc; (void)argv;
-
-	/* Dummy mode: start_gui uses map dims for sizing; free_game_content
-	** needs pointers to be NULL-safe. */
 	ft_memset(&game, 0, sizeof(t_game));
 
 	// init_game(&game);
